758cef4f2aa92b6ca68415735d61c007f532d184
Antoine Latgrand NDIAYE <antoinendiaye09@gmail.com>
2025-07-27 21:14:20 +0200

    feat: goal tracking backend repository initialization

---
+++ src/main/java/com/antoine/goaltrackerbackend/GoaltrackerbackendApplication.java
@@ -0,0 +1,13 @@
+package com.antoine.goaltrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoaltrackerbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoaltrackerbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antoine/goaltrackerbackend/GoaltrackerbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antoine.goaltrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoaltrackerbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
